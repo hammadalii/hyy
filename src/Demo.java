153cac52c7ef862cd96285bd4541cbf3894f8892
Hammad Alii <Hammad Alii@Hammad>
2017-10-22 19:14:01 +0500

    prototype commit

---
+++ src/Demo.java
@@ -0,0 +1,19 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person1 = new Person("Fred");
+		System.out.println("person 1:" + person1);
+		Person person2 = (Person) person1.doClone();
+		System.out.println("person 2:" + person2);
+
+		Dog dog1 = new Dog("Wooof!");
+		System.out.println("dog 1:" + dog1);
+		Dog dog2 = (Dog) dog1.doClone();
+		System.out.println("dog 2:" + dog2);
+
+	
+	}
+
+}
